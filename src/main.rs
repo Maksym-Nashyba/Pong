ee3d34ba3718049ceeef1d8d1b3d1b8e24f0ef91
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2023-03-30 11:22:33 +0200

    Extracted event loop from renderer.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use crate::renderer::Renderer;
+
 mod renderer;
 
 fn main() {
-    renderer::initialize_renderer();
+    let event_loop:EventLoop<()> = EventLoop::new();
+    let mut renderer:Renderer = renderer::initialize_renderer(&event_loop);
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            }
+            Event::WindowEvent {
+                event: WindowEvent::Resized(_),
+                ..
+            } => {
+                renderer.on_resized();
+            }
+            Event::RedrawEventsCleared => {
+                renderer.submit_draw();
+            }
+            _ => (),
+        }
+    });
 }
