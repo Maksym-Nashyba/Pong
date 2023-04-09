02e429db4c34134ccba64ae2e3d09771bc7086fd
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2023-04-10 00:36:57 +0200

    extracted rendering into a crate

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,10 @@
-use vulkano::memory::allocator::StandardMemoryAllocator;
+use winit::event_loop::EventLoop;
+use renderer::renderer::Renderer;
 use winit::event::{Event, WindowEvent};
-use winit::event_loop::{ControlFlow, EventLoop};
-use crate::material::Material;
-use crate::renderer::draw_call::DrawCall;
-use crate::renderer::Renderer;
-use crate::renderer::model::{Model, Vertex};
-use crate::renderer::shader_loader::ShaderType;
-use crate::transform::Transform;
 
-mod renderer;
-mod transform;
-mod material;
-
-fn main() {
+fn main(){
     let event_loop:EventLoop<()> = EventLoop::new();
-    let mut renderer:Renderer = renderer::initialize_renderer(&event_loop);
-
-
-    //              --DATA HERE FOR NOW--              //
-
-    let memory_allocator: StandardMemoryAllocator =
-        StandardMemoryAllocator::new_default(renderer.device.clone());
-
-    let vertices = vec![
-        Vertex {
-            position: [-0.75, 1.0, 0.5],
-        },
-        Vertex {
-            position: [0.0, -1.0, 0.5],
-        },
-        Vertex {
-            position: [0.4, 0.0, 0.5],
-        },
-        Vertex {
-            position: [-1.0, -0.5, 0.5],
-        },
-        Vertex {
-            position: [0.2, -0.5, 0.5],
-        },
-        Vertex {
-            position: [0.75, 1.0, 0.5],
-        },
-        Vertex {
-            position: [0.2, -0.5, 0.5],
-        },
-        Vertex {
-            position: [1.0, -0.5, 0.5],
-        },
-        Vertex {
-            position: [0.4, 0.0, 0.5],
-        },
-    ];
-
-    let transform:Transform = Transform::identity();
-    let model = Model::load(&memory_allocator, vertices);
-    let material = Material::new(&renderer,
-   renderer.shader_container.get_shader(ShaderType::Vertex, "direct").expect("Didn't find shader"),
- renderer.shader_container.get_shader(ShaderType::Fragment, "direct").expect("Didn't find shader"));
-
-    //              --END OF DEBUG DATA--              //
-
+    let mut renderer:Renderer = renderer::innit_renderer(&event_loop);
 
     event_loop.run(move |event, _, control_flow| {
         match event {
@@ -67,24 +12,18 @@ fn main() {
                 event: WindowEvent::CloseRequested,
                 ..
             } => {
-                *control_flow = ControlFlow::Exit;
-            }
+                control_flow.set_exit();
+            },
             Event::WindowEvent {
                 event: WindowEvent::Resized(_),
                 ..
             } => {
                 renderer.on_resized();
-            }
+            },
             Event::RedrawEventsCleared => {
-                let draw_calls = vec![
-                    DrawCall{
-                        transform:transform.clone(),
-                        model:model.clone(),
-                        material:material.clone()}];
-
-                renderer.submit_frame_draw(draw_calls);
-            }
-            _ => (),
+                //renderer.submit_frame();
+            },
+            _ => ()
         }
     });
 }
